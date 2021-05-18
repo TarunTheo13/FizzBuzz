2a0345a4b5cd6c696763a89b28802736abd871ee
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-05-18 18:02:32 +0100

    Committed with Working Solution

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
-require 'fizzbuzz'
+require "fizzbuzz"
 
-describe 'fizzbuzz' do
-  it 'return "fizz" when passed 3' do
-    expect (fizzbuzz(3)).to eq 'fizz'
+describe "fizzbuzz" do
+  it "return 'fizz' when passed 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "return 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "return 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
